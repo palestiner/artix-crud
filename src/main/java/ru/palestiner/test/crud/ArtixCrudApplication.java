5e61e31bff478e86f0231752a09e44ec23ba7876
Медведев Юрий Владимирович <muv@cs-consult.ru>
2021-02-01 14:59:59 +0700

    init

---
+++ src/main/java/ru/palestiner/test/crud/ArtixCrudApplication.java
@@ -0,0 +1,13 @@
+package ru.palestiner.test.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtixCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtixCrudApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/palestiner/test/crud/view/main/MainView.java
@@ -0,0 +1,7 @@
+package ru.palestiner.test.crud.view.main;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+
+
+public class MainView extends AppLayout {
+}
