9f8470bd737acb043fcb684c560049cc574de409
Медведев Юрий Владимирович <muv@cs-consult.ru>
2021-02-01 19:19:22 +0700

    populating the grid with data from the backend

--- src/main/java/ru/palestiner/test/crud/view/main/MainView.java
+++ src/main/java/ru/palestiner/test/crud/view/main/MainView.java
@@ -1,7 +1,35 @@
 package ru.palestiner.test.crud.view.main;
 
-import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import ru.palestiner.test.crud.entity.Customer;
+import ru.palestiner.test.crud.service.CustomerService;
 
 
-public class MainView extends AppLayout {
+@Route(value = "")
+public class MainView extends VerticalLayout {
+
+    private final CustomerService customerService;
+
+    private Grid<Customer> customerGrid = new Grid<>(Customer.class);
+
+    public MainView(CustomerService customerService) {
+        this.customerService = customerService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        add(customerGrid);
+        updateList();
+    }
+
+    private void configureGrid() {
+        customerGrid.addClassName("customer-grid");
+        customerGrid.setSizeFull();
+        customerGrid.setColumns("name", "gender", "dayOfBirth");
+    }
+
+    private void updateList() {
+        customerGrid.setItems(customerService.findAll());
+    }
 }
